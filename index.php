47ad00c72508cc74aba8d7a214d932db14361c5a
aqifahnaf <93071045+qifahnaf@users.noreply.github.com>
2026-03-09 04:27:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Survei Kepuasan</title>
+
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+<div class="container">
+
+<h1>DINAS PELAYANAN TERPADU</h1>
+
+<p class="ucapan">
+Silakan berikan penilaian pelayanan kami
+</p>
+
+<button class="puas" onclick="kirim('puas')">
+😊 PUAS
+</button>
+
+<button class="tidak" onclick="tidakPuas()">
+😞 TIDAK PUAS
+</button>
+
+</div>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
